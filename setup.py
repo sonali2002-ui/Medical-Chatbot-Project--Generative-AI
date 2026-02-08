88f827d7315aa95107abec6a452cadbba1cb88c3
Aniket Dhada <sonalidhadas9@gmail.com>
2026-02-08 14:58:27 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medibot",
+    version="0.1.0",
+    author="Sonali Dhadas",
+    author_email="sonalidhadas9@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
