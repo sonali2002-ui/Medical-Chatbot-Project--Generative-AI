16519a568cad9c7c37b150a955b20529cba8269b
Sonali Dhadas <sonalidhadas9@gmail.com>
2026-02-14 16:39:37 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="medibot",
-    version="0.1.0",
+    version="0.0.0",
     author="Sonali Dhadas",
     author_email="sonalidhadas9@gmail.com",
     packages=find_packages(),
